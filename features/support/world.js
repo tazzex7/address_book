88f9c4ce6051752f94a918a559c8afa18d67f8e1
Becca <beccaburns89@gmail.com>
2019-09-10 13:05:25 +0200

    Adds additional actions to scenario, Adds helpers and content to pass cucumber tests. Cucumber tests passes for 4 scenarios. Adds content to HTML page.

---
+++ features/support/world.js
@@ -0,0 +1,28 @@
+const { setWorldConstructor } = require('cucumber')
+const { expect } = require ('chai')
+const puppeteer = require('puppeteer')
+
+const HOME_PAGE = 'http://localhost:3000'
+
+class AddressBookWorld {
+    constructor() {}
+
+    async openHomePage() {
+        this.browser = await puppeteer.launch( {headless: false, slowmo: 100})
+        this.page = await this.browser.newPage()
+        await this.page.goto(HOME_PAGE)
+    }
+
+    async closeHomePage() {
+        await this.browser.close()
+    }
+
+    async pageHasTextContent(expectedContent) {
+        const pageContent = await this.page.content()
+        const actualContent = pageContent.match(expectedContent) [0]
+
+        expect(actualContent).to.be.eq(expectedContent)
+    }
+}
+
+setWorldConstructor(AddressBookWorld)
