a6def82c9106650e00a5ad8c3423b28907518ea6
Becca <beccaburns89@gmail.com>
2019-09-11 14:42:35 +0200

    Adds code for rendering contacts in local storage.Modifies files.

--- features/support/world.js
+++ features/support/world.js
@@ -57,6 +57,13 @@ class AddressBookWorld {
         )
         expect(actualCount) == (expectedCount)
     }
+
+    async pageDoesNotHaveTextContent(unexpectedContent) {
+        const pageContent = await this.page.content()
+        let actualContent = pageContent.match(unexpectedContent)
+
+        expect(actualContent).to.be.eq(null)
+    }
 }
 
 setWorldConstructor(AddressBookWorld)
